0df2d4b98beb1c90bfd4a0d471d15fcd66bd6176
Rafael Teodosio <rafael.teodosio@plathanus.tech>
2021-11-01 00:31:17 -0300

    Definindo por triangulacao o delimitador
    Co-authored-by: Kalebe Lopes <calbkalebe@gmail.com>
    Co-authored-by: Vitor Alves <vitor.alves07750@outlook.com>

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -1,6 +1,7 @@
 package main;
 
 import exceptions.ArquivoNaoEncontradoException;
+import exceptions.DelimitadorInvalidoException;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,38 +9,53 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class Parser {
-    Vector <Vector<Double>> buffer;
+	private Vector <Vector <Double>> buffer;
+	private char delimitador;
+	
+	public Parser(){
+		buffer = new Vector <Vector <Double>>();
+		delimitador = ';';
+	}
 
-    public Parser(){
-        buffer = new Vector <Vector <Double>>();
-    }
+	public void lerArquivo(String path) throws ArquivoNaoEncontradoException {
+		Scanner input;
+		try {
+			input = new Scanner(new FileReader(path));
+		} catch (FileNotFoundException e) {
+			throw new ArquivoNaoEncontradoException(path);
+		}
 
-    public void lerArquivo(String path) throws ArquivoNaoEncontradoException {
-        Scanner input;
-        try {
-            input = new Scanner(new FileReader(path));
-        } catch (FileNotFoundException e) {
-            throw new ArquivoNaoEncontradoException(path);
-        }
+		while(input.hasNextLine()) {
+			
+			String data = input.nextLine();
+			
+			if (data.startsWith("-")) {
+				Vector<Double> row = new Vector<Double>();
+				buffer.add(row);
+			}
+			else {
+				buffer.lastElement().add(Double.parseDouble(data));
+			}
+		}
+		
+		input.close();
+		
+	}
 
-        while(input.hasNextLine()) {
+	public Vector <Vector <Double>> getBuffer() {
+		return buffer;
+	}
 
-            String data = input.nextLine();
-
-            if (data.startsWith("-")) {
-                Vector<Double> row = new Vector<Double>();
-                buffer.add(row);
-            }
-            else {
-                buffer.lastElement().add(Double.parseDouble(data));
-            }
-        }
-
-        input.close();
-
-    }
-
-    public Vector <Vector <Double>> getBuffer() {
-        return buffer;
-    }
+	public void setDelimitador(String delimitador) throws DelimitadorInvalidoException {
+		if (delimitador.length() == 1) {
+			this.delimitador = delimitador.charAt(0);
+		}
+		else {
+			throw new DelimitadorInvalidoException(delimitador);
+		}
+	}
+	
+	public char getDelimitador() {
+		return delimitador;
+	}
 }
