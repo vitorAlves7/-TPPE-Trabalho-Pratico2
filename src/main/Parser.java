20a61135470f10791c90b7848ed1d4540bc4fffd
vitorAlves7 <vitor.alves07750@outlook.com>
2021-11-01 18:25:38 -0300

    Adicionando o testes de escrita de arquivo e as funções de escrita de arquivo
    Co-authored-by: Kalebe Lopes <calbkalebe@gmail.com>
    Co-authored-by: Rafael Teodosio <rafzteodosio@gmail.com>

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -1,7 +1,6 @@
 package main;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,6 +15,8 @@ public class Parser {
 	private Vector <Vector <Double>> buffer;
 	private char delimitador;
 	private String caminhoArquivoSaida;
+	private String nomeArquivoEntrada;
+	private String arquivoSaida;
 	private int formato;
 	public static final int COLUNA = 0;
 	public static final int LINHA = 1;
@@ -48,6 +49,12 @@ public class Parser {
 		}
 
 		input.close();
+		int lastIndex = path.lastIndexOf('/');
+		if(lastIndex != -1) {
+			this.nomeArquivoEntrada = path.substring(lastIndex+1);
+		} else {
+			this.nomeArquivoEntrada = path;
+		}
 
 	}
 
@@ -93,5 +100,81 @@ public class Parser {
 
 	public int getFormatoSaida() {
 		return formato;
+	}
+
+	public void escreverArquivo() throws EscritaNaoPermitidaException {
+		String caminhoCompletoSaida = this.nomeArquivoEntrada;
+
+		int indexPonto = -1;
+		for(int i = caminhoCompletoSaida.length() - 1; i >= 0; i--) {
+			if(caminhoCompletoSaida.charAt(i) == '.') {
+				indexPonto = i;
+				break;
+			}
+		}
+
+		if(indexPonto == 0) {
+			caminhoCompletoSaida = "Tab" + caminhoCompletoSaida;
+		} else if(indexPonto != -1) {
+			caminhoCompletoSaida = caminhoCompletoSaida.substring(0,indexPonto) + "Tab" + caminhoCompletoSaida.substring(indexPonto);
+		} else {
+			caminhoCompletoSaida += "Tab";
+		}
+
+		caminhoCompletoSaida = caminhoArquivoSaida + caminhoCompletoSaida;
+		this.arquivoSaida = caminhoCompletoSaida;
+
+		try {
+			File file = new File(caminhoCompletoSaida);
+			if(!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter filew = new FileWriter(file);
+			BufferedWriter bufferw = new BufferedWriter(filew);
+
+			if(this.formato == LINHA) {
+				for(int index = 0; index < this.buffer.size(); index++) {
+					if(index != 0) bufferw.newLine();
+					bufferw.write(Double.toString(index+1));
+					for (int i=0; i < this.buffer.elementAt(index).size(); i++) {
+						bufferw.write(this.delimitador);
+						bufferw.write(Double.toString(buffer.elementAt(index).elementAt(i)));
+					}
+				}
+			} else if(this.formato == COLUNA) {
+				Vector <Vector <String>> linhas = new Vector< Vector <String>>();
+				int max_size=0;
+				for(int i=0; i<buffer.size(); i++) {
+					if(i!=0) bufferw.write(this.delimitador);
+					bufferw.write(Double.toString(i+1));
+					bufferw.write(Double.toString(i+1));
+					if(buffer.elementAt(i).size() > max_size) max_size = buffer.elementAt(i).size();
+				}
+				bufferw.newLine();
+				for(int j=0; j<max_size; j++) { // linha
+					if(j!=0) bufferw.newLine();
+					for(int i=0; i<buffer.size(); i++) {
+						if(i!=0) bufferw.write(this.delimitador);
+						if(buffer.elementAt(i).size() > j) {
+							bufferw.write(Double.toString(buffer.elementAt(i).elementAt(j)));
+						}
+					}
+				}
+
+			} else {
+//		    	throw new FormatoInvalidoException(this.formato);
+			}
+
+			bufferw.close();
+			filew.close();
+		} catch (Exception e) {
+			System.out.println(e);
+			throw new EscritaNaoPermitidaException(caminhoCompletoSaida);
+		}
+	}
+
+	public String getArquivoSaida() {
+		return arquivoSaida;
 	}
 }
